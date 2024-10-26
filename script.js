36a8acf050ad061c735e5b3c6a9f3486bc337f48
cyb3rj41 <167119575+cyberhelloff@users.noreply.github.com>
2024-10-26 23:18:40 +0530

    Add files via upload

---
+++ script.js
@@ -0,0 +1,86 @@
+function one(){
+     document.getElementById("pic").src="https://www.icegif.com/wp-content/uploads/love-heart-icegif.gif";
+}
+
+function two(){
+   document.getElementById("gap").style.display = "none";
+}
+
+function three(){
+  var a = document.getElementById("gap").value;
+  document.getElementById("cont").style.backgroundColor = a;
+}
+
+function four(){
+    document.getElementById("clickme").style.display = "none";
+}
+
+function five(){
+    document.body.style.backgroundImage = "url('https://images6.alphacoders.com/118/1184847.jpg')";
+}
+
+function six(){
+  document.getElementById("box").style.display = "block";
+}
+
+function seven(){
+         document.getElementById("pic").src="https://media.tenor.com/kKUc2ijAJg8AAAAC/heart-cute.gif";
+}
+
+function eight(){
+    document.getElementById("res").innerHTML="I love you too &#x2764;";
+}
+
+function nine(){
+    document.body.style.backgroundImage = "url('https://edge.mwallpapers.com/photos/wallpapers/anime/anime-love-hot-kissing-hd-wallpaper-wallpaper-new-hd-wallpaper-android-iphone-hd-wallpaper-background-downloadhd-wallpapers-desktop-background-android-iphone-1080p-4k-treiw.jpg')";
+}
+
+function ten(){
+  document.getElementById("res").style.fontColor="white";
+}
+
+function eleven(){
+    document.getElementById("yes").style.display = "none";
+    document.getElementById("no").style.display = "none";
+}
+
+function twelve(){
+   document.getElementById("quote").style.display = "block";
+}
+
+function broken(){
+  document.getElementById("res").innerHTML="  &#x1F494;"
+}
+
+function heart(){
+   document.getElementById("pic").src="https://media.tenor.com/EAx58J383ocAAAAi/heart-break-heart-broken.gif";
+}
+
+function bg(){
+   document.body.style.backgroundImage = "url('https://wallpapers.com/images/featured/5jm8to8m3a6wgmds.jpg')";
+}
+
+function luv(){
+  one();
+  two();
+  three();
+  four();
+  five();
+  six();
+}
+
+function yes(){
+   seven();
+   eight();
+  nine();
+  ten();
+  eleven();
+  twelve();
+}
+
+function no(){
+  broken();
+  heart();
+  bg();
+  eleven();
+}
